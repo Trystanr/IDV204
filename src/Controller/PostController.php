3eec02ef3456441a546f2dc08b622681ea1e0f3d
LukeRamsay <43437616+LukeRamsay@users.noreply.github.com>
2019-10-18 15:46:21 +0200

    First Commit
    Added the base folder with login/signup and displaying of questions

---
+++ src/Controller/PostController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * * @Route("/post", name="post.")
+*/
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(PostRepository $postRepository)
+    {
+
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @return Response
+     */
+    public function create(Request $request){
+        // create a new post with title
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+        $form->getErrors();
+        if($form->isSubmitted()){
+             //entity manager
+            
+            $em = $this->getDoctrine()->getManager();
+            //** @var UploadedFile $file  */
+            $file = $request->files->get('post')['attachment'];
+
+            if($file){
+                $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
+                $file->move(
+                    $this->getParameter('uploads_dir'),
+                    $filename
+                );
+
+                $post->setImage($filename);
+                $em->persist($post);
+                $em->flush();
+            }
+            
+
+            return $this->redirect($this->generateUrl('post.index'));
+        }
+
+       
+
+        
+
+        
+
+        //return a response
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @return Response
+     */
+    public function show(Post $post){
+        //create the show view
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @return Response
+     */
+    public function remove(Post $post){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was removed');
+        return $this->redirect($this->generateUrl('post.index'));
+
+    }
+}
